9683c61dcf64ef1b38f783dab5146c9ba6a54bbb
Daniel <danielnijkamp2004@gmail.com>
2022-03-24 19:20:14 +0100

    Added fingerpointing raycast

--- Special_IO/Assets/Scripts/UI_Script.cs
+++ Special_IO/Assets/Scripts/UI_Script.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Leap;
+using Leap.Unity;
 
 public class UI_Script : MonoBehaviour
 {
     public Leap.Unity.Interaction.InteractionSlider UI_Slider;
     public GameObject Desktop_rig;
-   
+    private bool ButtonPressed;
+
+    private void Start()
+    {
+        ButtonPressed = false;
+    }
     void Update()
     {
         if (Desktop_rig != null)
@@ -14,6 +21,37 @@ public class UI_Script : MonoBehaviour
             Desktop_rig.transform.rotation = Quaternion.Euler(Desktop_rig.transform.rotation.x, UI_Slider.HorizontalSliderValue, Desktop_rig.transform.rotation.z);
             Desktop_rig.transform.position = new Vector3((UI_Slider.HorizontalSliderValue / 260), Desktop_rig.transform.position.y, -(UI_Slider.HorizontalSliderValue / 260) + 0.9f);
         }
-        
+        var hand = Hands.Right;
+        if (hand != null)
+        {
+            if (ButtonPressed)
+            {
+                Debug.DrawRay(hand.Fingers[1].TipPosition.ToVector3(), hand.Fingers[1].Direction.ToVector3(), Color.red);
+                Debug.Log(hand.Fingers[1].TipPosition.ToVector3());
+            }
+            
+        }
+    }
+    
+    void Activate_Finger_Point()
+    {
+        Debug.Log("Activated");
+    }
+    void Deactivate_Finger_Point()
+    {
+        Debug.Log("Deactivated");
+    }
+    public void DetectButtonInput()
+    {
+        if (!ButtonPressed)
+        {
+            Activate_Finger_Point();
+            ButtonPressed = true;
+        }
+        else if (ButtonPressed)
+        {
+            Deactivate_Finger_Point();
+            ButtonPressed = false;
+        }
     }
 }
